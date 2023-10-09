23b31aa2dc84586c28d78b145ce1e09a9d4ecb5f
NgoDinhCanh <rapidstars97@gmail.com>
2023-10-09 14:53:48 +0700

    feat : add comment table and remove service attachment

---
+++ db/migrations/000003_init_commentdb.up.sql
@@ -0,0 +1,23 @@
+START TRANSACTION;
+CREATE SCHEMA IF NOT EXISTS "comment";
+
+CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
+
+CREATE TABLE
+    comment.comments (
+        id uuid NOT NULL DEFAULT (uuid_generate_v4()),
+        user_id uuid NOT NULL,
+        content text NOT NULL,
+        reply_to string DEFAULT NULL,
+        post_id uuid NOT NULL,
+        parent_comment_id uuid DEFAULT NULL,
+        created_at timestamp with time zone NOT NULL DEFAULT current_timestamp,
+        updated_at timestamp with time zone NOT NULL DEFAULT (now()),
+        deleted_at timestamp with time zone NULL,
+        CONSTRAINT pk_comments PRIMARY KEY (id)
+    );
+
+CREATE INDEX ix_user_id ON comment.comments (user_id);
+CREATE INDEX ix_post_id ON comment.comments (post_id);
+CREATE INDEX ix_parent_comment_id ON comment.comments (parent_comment_id);
+COMMIT;
