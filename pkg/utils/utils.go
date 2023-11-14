783ed9bbdf107d45ffe0e5b90724cdda7099fdb6
dinhcanh303 <Rapidstars97@gmail.com>
2023-11-14 09:24:49 +0700

    minor

--- pkg/utils/utils.go
+++ pkg/utils/utils.go
@@ -71,7 +71,7 @@ func ConvertArStringToArNullUUID(strings []string) ([]uuid.NullUUID, error) {
 }
 func LoadFileEnvOnLocal() error {
 
-	err := godotenv.Load("../../.env.test")
+	err := godotenv.Load("../../.env")
 	if err != nil {
 		return err
 	}
