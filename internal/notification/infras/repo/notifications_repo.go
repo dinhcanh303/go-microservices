517d0ae4be6c0be5d472b651a079fdd3d6a203ec
dinhcanh303 <Rapidstars97@gmail.com>
2024-02-01 17:17:12 +0700

    feat : using gorm in service notifications

--- internal/notification/infras/repo/notifications_repo.go
+++ internal/notification/infras/repo/notifications_repo.go
@@ -1,20 +1,46 @@
 package repo
 
 import (
+	"context"
+
+	"github.com/dinhcanh303/go-microservices/internal/notification/domain"
 	"github.com/dinhcanh303/go-microservices/internal/notification/usecases/notifications"
-	"github.com/dinhcanh303/go-microservices/pkg/postgres"
+	"github.com/google/uuid"
 	"github.com/google/wire"
+	"github.com/pkg/errors"
+	"gorm.io/gorm"
 )
 
 type notificationRepo struct {
-	pg postgres.DBEngine
+	db *gorm.DB
+}
+
+// GetNotiByUserId implements notifications.NotificationRepo.
+func (rp *notificationRepo) GetNotiByUserId(ctx context.Context, userId uuid.UUID, options string) ([]*domain.Notification, error) {
+	var results []*domain.Notification
+	db := rp.db.Table("noti.notifications").Where(
+		"receiver_id @> ? AND NOT ? <@read_id",
+		[]string{userId.String()}, []string{userId.String()})
+	if err := db.Select("*").Offset(0).Limit(20).Find(&results).Error; err != nil {
+		return nil, errors.Wrap(err, "repo.GetNotiByUserId failed")
+	}
+	return results, nil
+
+}
+
+// UpsertNoti implements notifications.NotificationRepo.
+func (rp *notificationRepo) UpsertNoti(ctx context.Context, noti *domain.Notification) error {
+	if err := rp.db.Table("noti.notifications").Create(noti).Error; err != nil {
+		return errors.Wrap(err, "create noti failed")
+	}
+	return nil
 }
 
 var RepositorySet = wire.NewSet()
 
-func NewNotificationRepo(pg postgres.DBEngine) notifications.NotificationRepo {
+func NewNotificationRepo(db *gorm.DB) notifications.NotificationRepo {
 	return &notificationRepo{
-		pg: pg,
+		db: db,
 	}
 }
 
--- internal/notification/usecases/notifications/interface.go
+++ internal/notification/usecases/notifications/interface.go
@@ -1,7 +1,19 @@
 package notifications
 
+import (
+	"context"
+
+	"github.com/dinhcanh303/go-microservices/internal/notification/domain"
+	"github.com/google/uuid"
+)
+
 type NotificationRepo interface {
+	GetNotiByUserId(ctx context.Context, userId uuid.UUID, options string) ([]*domain.Notification, error)
+	UpsertNoti(ctx context.Context, noti *domain.Notification) error
 }
 
 type UseCase interface {
+	UpsertNoti(ctx context.Context, noti *domain.Notification) error
+	GetNotiReadByUserId(ctx context.Context, userId uuid.UUID) error
+	GetNotiUnReadByUserId(ctx context.Context, userId uuid.UUID) error
 }
