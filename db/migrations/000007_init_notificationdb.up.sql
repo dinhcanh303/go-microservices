22d68ea99b8731af09e88e38430fd29a69ad4095
dinhcanh303 <rapidstars97@gmail.com>
2024-03-12 17:39:36 +0700

    feat : add table notifications service noti

--- db/migrations/000007_init_notificationdb.up.sql
+++ db/migrations/000007_init_notificationdb.up.sql
@@ -6,12 +6,12 @@ CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
 CREATE TABLE
     noti.notifications (
         id SERIAL PRIMARY KEY,
-        actor_id uuid [] DEFAULT NULL,
-        sender_id uuid [] DEFAULT NULL,
+        actor_id VARCHAR(255) NOT NULL DEFAULT NULL,
+        sender_id VARCHAR(255) DEFAULT NULL,
         data json DEFAULT NULL,
         type VARCHAR(255) DEFAULT NULL,
         object_type VARCHAR(255) DEFAULT NULL,
-        object_id uuid DEFAULT NULL,
+        object_id VARCHAR(255) DEFAULT NULL,
         read_at timestamp DEFAULT NULL,
         created_at timestamp with time zone NOT NULL DEFAULT current_timestamp,
         updated_at timestamp with time zone NOT NULL DEFAULT (now())
