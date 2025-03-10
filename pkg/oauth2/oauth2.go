d5230410efe256752c29cb7f771f5df732a3515d
Ngo Dinh Canh <rapidstars97@gmail.com>
2025-03-10 21:39:31 +0700

    Fix : log oauth2

--- pkg/oauth2/oauth2.go
+++ pkg/oauth2/oauth2.go
@@ -6,8 +6,9 @@ import (
 
 	"github.com/coreos/go-oidc/v3/oidc"
 	"github.com/dinhcanh303/go-microservices/pkg/constant"
-	"github.com/golang/glog"
+	"golang.org/x/exp/slog"
 	"golang.org/x/oauth2"
+	facebookOAuth2 "golang.org/x/oauth2/facebook"
 	googleOAuth2 "golang.org/x/oauth2/google"
 )
 
@@ -34,15 +35,15 @@ func InitOAuth() error {
 	ctx := context.Background()
 	googleClientID, ok := os.LookupEnv(constant.KeyGoogleClientID)
 	if !ok || len(googleClientID) == 0 {
-		glog.Fatalf("environment variable not declared: %s", constant.KeyGoogleClientID)
+		slog.Error("Environment variable not declared: %s", constant.KeyGoogleClientID)
 	}
 	googleClientSecret, ok := os.LookupEnv(constant.KeyGoogleClientSecret)
 	if !ok || len(googleClientSecret) == 0 {
-		glog.Fatalf("environment variable not declared: %s", constant.KeyGoogleClientSecret)
+		slog.Error("Environment variable not declared: %s", constant.KeyGoogleClientSecret)
 	}
 	googleUrlCallback, ok := os.LookupEnv(constant.KeyGoogleUrlCallback)
 	if !ok || len(googleClientSecret) == 0 {
-		glog.Fatalf("environment variable not declared: %s", constant.KeyGoogleUrlCallback)
+		slog.Error("Environment variable not declared: %s", constant.KeyGoogleUrlCallback)
 	}
 	if googleClientID != "" && googleClientSecret != "" {
 		p, err := oidc.NewProvider(ctx, "https://accounts.google.com")
@@ -53,36 +54,27 @@ func InitOAuth() error {
 		OAuthProviders.GoogleConfig = &oauth2.Config{
 			ClientID:     googleClientID,
 			ClientSecret: googleClientSecret,
-			RedirectURL:  "/oauth_callback/google",
-			Endpoint:     OIDCProviders.GoogleOIDC.Endpoint(),
-			Scopes:       []string{oidc.ScopeOpenID, "profile", "email"},
-		}
-
-		OAuthProviders.GoogleConfig = &oauth2.Config{
-			ClientID:     googleClientID,
-			ClientSecret: googleClientSecret,
 			RedirectURL:  googleUrlCallback,
 			Endpoint:     googleOAuth2.Endpoint,
-			Scopes:       []string{"email", "profile"},
+			Scopes:       []string{oidc.ScopeOpenID, "email", "profile"},
 		}
 	}
-	// facebookClientID, ok := os.LookupEnv(constant.KeyFacebookClientID)
-	// if !ok || len(facebookClientID) == 0 {
-	// 	glog.Fatalf("environment variable not declared: %s", constant.KeyFacebookClientID)
-	// }
-	// facebookClientSecret, ok := os.LookupEnv(constant.KeyFacebookClientSecret)
-	// if !ok || len(facebookClientSecret) == 0 {
-	// 	glog.Fatalf("environment variable not declared: %s", constant.KeyFacebookClientSecret)
-	// }
-	// if facebookClientID != "" && facebookClientSecret != "" {
-	// 	OAuthProviders.FacebookConfig = &oauth2.Config{
-	// 		ClientID:     facebookClientID,
-	// 		ClientSecret: facebookClientSecret,
-	// 		RedirectURL:  "/oauth_callback/facebook",
-	// 		Endpoint:     facebookOAuth2.Endpoint,
-	// 		Scopes:       []string{"public_profile", "email"},
-	// 	}
-	// }
-
+	facebookClientID, ok := os.LookupEnv(constant.KeyFacebookClientID)
+	if !ok || len(facebookClientID) == 0 {
+		slog.Error("Environment variable not declared: %s", constant.KeyFacebookClientID)
+	}
+	facebookClientSecret, ok := os.LookupEnv(constant.KeyFacebookClientSecret)
+	if !ok || len(facebookClientSecret) == 0 {
+		slog.Error("Environment variable not declared: %s", constant.KeyFacebookClientSecret)
+	}
+	if facebookClientID != "" && facebookClientSecret != "" {
+		OAuthProviders.FacebookConfig = &oauth2.Config{
+			ClientID:     facebookClientID,
+			ClientSecret: facebookClientSecret,
+			RedirectURL:  "/oauth_callback/facebook",
+			Endpoint:     facebookOAuth2.Endpoint,
+			Scopes:       []string{"public_profile", "email"},
+		}
+	}
 	return nil
 }
